efbabf901e30c16fdf07a8162167a4f32834af4a
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-08-01 20:26:51 +0100

    Saving for the night.

--- src/spells/Card.js
+++ src/spells/Card.js
@@ -11,26 +11,34 @@ const Card = ({ slug, isOpen, name, desc, img, range, duration, castingTime, lev
       <Link to={`/spells/${slug}`}>
         <div className={`card-header has-background-${cardStyle.schoolColour[school]}`}>
           <div className="card-header-title">
-            {name}
-            {icons.map(icon =>
-              <i key={icon} className={`${cardStyle.icons[icon]}`}></i>
-            )}
+            <div className="level">
+              <div className="level-left">
+                <div className="level-item">
+                  {name}
+                </div>
+              </div>
+              <div className="level-right">
+                <div className="level-item">
+                  {icons.map(icon =>
+                    <i key={icon} className={`${cardStyle.icons[icon]}`}></i>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Link>
       <div className="card-content">
         <div className="content">
-          <p>{isOpen ? desc : `${desc.slice(0,150)}...`}</p>
-          <button
-            className="button"
-            onClick={toggleDesc ? () => toggleDesc(isOpen, slug) : null}
-          >Expand</button>
+          <div>
+            <p onClick={toggleDesc ? () => toggleDesc(isOpen, slug) : null}>{isOpen ? desc : `${desc.slice(0,150)}...`}</p>
+          </div>
           <img src={img} />
-          <p>{range}</p>
-          <p>{duration}</p>
-          <p>{castingTime}</p>
-          <p>{level}</p>
-          <p>{school}</p>
+          <p>{`Range: ${range}`}</p>
+          <p>{`Duration: ${duration}`}</p>
+          <p>{`Casting time ${range}`}</p>
+          <p>{`Level: ${level}`}</p>
+          <p>{`Casting time: ${range}`}</p>
         </div>
       </div>
     </div>
