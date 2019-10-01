a73c3d5e618165788340008fcd2f62ebb9b720c1
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-10-01 09:41:17 +0100

    Spelling correction on splash page.

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -5,7 +5,7 @@ const Splash = () => {
   return (
     <section className="section">
       <div className="container">
-        <h1 className="title is-1 has-text-centered">D&D Spellbook</h1>
+        <h1 className="title is-1 has-text-centered">D&D Spell Book</h1>
         <h2 className="subtitle is-3 has-text-centered"><img src="https://media.giphy.com/media/12NUbkX6p4xOO4/giphy.gif" /></h2>
         <Link to="/spells">
           <div className="columns is-centered">
